59f18578e7ac48c7a79ebc828b5449a63985779d
Jonas Stein <news@jonasstein.de>
2016-04-11 07:36:12 +0200

    filename generation works

---
+++ pybibtexcleaner.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+
+import bibtexparser
+import os.path
+import re
+import argparse
+
+def remove_ce(mystring):
+    #regex = re.compile(r"ce", re.IGNORECASE)    
+    cleanedstring = re.sub(r"\\ce", "", mystring)
+    return(cleanedstring)    
+
+def cleantitle(title):
+    title = remove_ce(title)
+    title = re.sub(r"{", "", title)
+    title = re.sub(r"}", "", title)
+    mycleantitle = re.sub(r" ", "_", title)
+    return(mycleantitle)
+
+def newfilename(myitem):
+    filename = "%s-%s"%(myitem["ID"],cleantitle(myitem["title"]))
+    return(filename)
+
+#https://docs.python.org/3/howto/argparse.html
+with open('literature.bib') as bibtex_file:
+    bibtex_str = bibtex_file.read()
+
+bib_database = bibtexparser.loads(bibtex_str)
+#print(bib_database.entries)
+
+for item in bib_database.entries:
+    if item["ENTRYTYPE"]=='article':
+        print(item["ID"])
+
+        if 'file' in item:
+        
+            filename = item["file"].split(":")[1]
+            print("new filename: %s"%newfilename(item))
+            ## If newfilename is alphanum only, then rename 
+            #if os.path.isfile(filename):
+            #    print("**Found:   ", filename)
+            #else:    
+            #    print("**Missing: ", filename)
+        
+        else:
+            print("No file.")
+
+
+        
+#            if item["title"]:
+                #print(item["title"])
+#            else:
+#                print("Warning: Title is missing!")
+    #,  item["title"], item["file"])
+    
