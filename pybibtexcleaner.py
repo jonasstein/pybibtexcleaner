178b565eb4bc078427b94a94f0cc445d7641fbb2
Jonas Stein <news@jonasstein.de>
2016-04-13 23:43:59 +0200

    clean title and file copy works fine now

--- pybibtexcleaner.py
+++ pybibtexcleaner.py
@@ -1,50 +1,75 @@
 #!/usr/bin/env python3
 
 import bibtexparser
-import os.path
+import os
 import re
-import argparse
 import sys
+import string
+import shutil
 
 def remove_ce(mystring):
-    #regex = re.compile(r"ce", re.IGNORECASE)    
     cleanedstring = re.sub(r"\\ce", "", mystring)
     return(cleanedstring)    
 
 def cleantitle(title):
     title = remove_ce(title)
-    title = re.sub(r"{", "", title)
-    title = re.sub(r"}", "", title)
-    mycleantitle = re.sub(r" ", "_", title)
-    return(mycleantitle)
+    title = re.sub(r" ", "_", title)
+    title = re.sub(r"ä", "ae", title)
+    title = re.sub(r"ö", "oe", title)
+    title = re.sub(r"ü", "ue", title)
+    title = re.sub(r"ß", "ss", title)
+    title = re.sub(r"Ä", "Ae", title)
+    title = re.sub(r"Ö", "Oe", title)
+    title = re.sub(r"Ü", "Ue", title)
+    title = re.sub(r"µ", "mu", title)
+    
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+    validtitle = ''
+    for c in title:
+        if (c in valid_chars):
+            validtitle = '%s%s'%(validtitle,c)
+    return(validtitle)
 
-def newfilename(myitem):
-    filename = "%s-%s"%(myitem["ID"],cleantitle(myitem["title"]))
+def newfilename(myitem, file_extension):
+    basefilename = "%s-%s"%(myitem["ID"],cleantitle(myitem["title"]))
+    print(basefilename)    
+    filename = "%s%s"%(basefilename, file_extension.lower())
     return(filename)
 
-with open(sys.argv[1]) as bibtex_file:
-    bibtex_str = bibtex_file.read()
+def isfilename(teststring):
+    # return true, if only numbers and characters
+    valid_chars = "-_.%s%s" % (string.ascii_letters, string.digits)
+    onlyvalidchar = True    
+    for c in teststring:
+        onlyvalidchar = onlyvalidchar and (c in valid_chars)
+    return onlyvalidchar
 
-bib_database = bibtexparser.loads(bibtex_str)
-#print(bib_database.entries)
-
-for item in bib_database.entries:
-    if item["ENTRYTYPE"]=='article':
-        print(item["ID"])
-
-        if 'file' in item:
-        
-            filename = item["file"].split(":")[1]
-            print("new filename: %s"%newfilename(item))
-            ## If newfilename is alphanum only, then rename 
-            #if os.path.isfile(filename):
-            #    print("**Found:   ", filename)
-            #else:    
-            #    print("**Missing: ", filename)
-        
-        else:
-            print("No file.")
-
+    
+if __name__ == "__main__":
+    with open(sys.argv[1]) as bibtex_file:
+        bibtex_str = bibtex_file.read()
+    
+    bib_database = bibtexparser.loads(bibtex_str)
+    #print(bib_database.entries)
+    
+    for item in bib_database.entries:
+        if item["ENTRYTYPE"]=='article':
+            print(item["ID"])
+    
+            if 'file' in item:
+                fullpathtopaper = item["file"].split(":")[1]
+                filename, file_extension = os.path.splitext(fullpathtopaper)
+                #print(filename, file_extension)
+                print("old filename: %s"%fullpathtopaper)
+                mynewfilename = newfilename(item, file_extension)
+                print("new filename: %s"%mynewfilename)
+                                
+                if os.path.isfile(fullpathtopaper):
+                    shutil.copy2(fullpathtopaper,"/tmp/test/%s"%mynewfilename)
+                
+            else:
+                print("No file.")
+    
 
         
 #            if item["title"]:
